c8d59d13ad287a47e8dc198ed9a80118889ba686
TimotyDarrelDavidson <c14230235@john.petra.ac.id>
2024-12-18 17:03:25 +0700

    benerin BST

--- BST.py
+++ BST.py
@@ -1,65 +1,72 @@
 from Video import Video  
 
+class BSTnode:
+    def __init__(self, video: Video):
+        self.video = video
+        self.left = None
+        self.right = None
+
 class BST:
 	def __init__(self) -> None:
 		self.root = None
 
-	def insert(self, value, tag, description, duration):
-		def insertDFS(current, value, tag, description, duration):
-			if current is None:
-				return Video(value, tag, description, duration)
-			if value < current.title:
-				current.left = insertDFS(current.left, value, tag, description, duration)
-			elif value > current.title:
-				current.right = insertDFS(current.right, value, tag, description, duration)
-			return current  
+	def insert(self, video: Video):
+		def insertDFS(node: BSTnode, video: Video):
+			if video.title < node.video.title:
+				if node.left:
+					insertDFS(node.left, video)
+				else:
+					node.left = BSTnode(video)
+			elif video.title > node.video.title:
+				if node.right:
+					insertDFS(node.right, video)
+				else:
+					node.right = BSTnode(video)
 
 		if self.root is None:
-			self.root = Video(value, tag, description, duration)
+			self.root = BSTnode(video)
 		else:
-			insertDFS(self.root, value, tag, description, duration)
+			insertDFS(self.root, video)
 
 	def printAllTree(self):
-		def DFS(current: Video):
+		def DFS(current: BSTnode):
 			if current is None:
 				return
 			# Debugging: Print current.left
 			if current.left is not None:
-				print(f"current.left = {current.left.title}")
+				print(f"current.left = {current.left.video.title}")
 			else:
 				print("current.left = None")
 			
 			DFS(current.left)
 			
 			# Print the current node's title
-			print(f"current.title = {current.title}", end=" ")
+			print(f"current.title = {current.video.title}", end=" ")
 			
 			# Debugging: Print current.right
 			if current.right is not None:
-				print(f"current.right = {current.right.title}")
+				print(f"current.right = {current.right.video.title}")
 			else:
 				print("current.right = None")
 			
 			DFS(current.right)
 		DFS(self.root)
 
-	def search(self, value):
-		def DFS(current, value):
+	def search(self, title):
+		def DFS(current, title):
 			if current is None:
 				return None
-			if current.title == value:
+			if current.title == title:
 				return current
-			elif value < current.title:
-				return DFS(current.left, value)
+			elif title < current.title:
+				return DFS(current.left, title)
 			else:
-				return DFS(current.right, value)
-		return DFS(self.root, value)
+				return DFS(current.right, title)
+		return DFS(self.root, title)
 	
-if __name__ == "__main__":
-    tree = BST()
-    tree.insert("Video1", ["tag1","tag2","tag3"], "desc1", 60 * 3)
-    tree.insert("Video2", ["tag1","tag2","tag3"], "desc1", 60 * 3)
-    tree.insert("Video3", ["tag1","tag2","tag3"], "desc1", 60 * 3)
-    tree.insert("Video4", ["tag1","tag2","tag3"], "desc1", 60 * 3)
+# if __name__ == "__main__":
+#     tree = BST()
+#     tree.insert("Video1", ["tag1","tag2","tag3"], "desc1", 60 * 3)
+#     tree.insert("Video2", ["tag1","tag2","tag3"], "desc1", 60 * 3)
     
-    tree.printAllTree()
+#     tree.printAllTree()
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import BST
+from BST import BST
+from Video import Video
 import History
 import Queue
+from Graph import Recommendation
 
 def main_menu():
   print("WELCOME TO STREAMTREE")
@@ -10,30 +12,66 @@ def main_menu():
   print("3. Find recommended")
   print("4. Add video ke Queue")
   print("5. Remove video dari Queue")
+  print("6. Display Recommendation for current Video")
+  print("7. Display All Video")
   print("6. Exit")
   print()
+  
+  videos = BST()
+  recommendation = Recommendation()
+  
+  video1 = Video('Video1', ['tag1','tag2'], 'This is Video1', 60)
+  video2 = Video('Video2', ['tag1','tag2'], 'This is Video2', 60)
+  video3 = Video('Video3', ['tag2','tag3'], 'This is Video3', 60)
+  video4 = Video('Video4', ['tag3','tag4'], 'This is Video4', 60)
+  
+  videos.insert(video1)
+  videos.insert(video2)
+  videos.insert(video3)
+  videos.insert(video4)
+  recommendation.addVideo(video1)
+  recommendation.addVideo(video2)
+  recommendation.addVideo(video3)
+  recommendation.addVideo(video4)
+  
 
-  try:
-    choice = int(input("Pilih menu : "))
-    if choice == 1:
-      print("Fungsi Add video dipilih.")
-    elif choice == 2:
-      print("Fungsi Remove video dipilih.")
-    elif choice == 3:
-      print("Fungsi Find recommended dipilih.")
-    elif choice == 4:
-      print("Fungsi Add video ke Queue dipilih.")
-    elif choice == 5:
-      print("Fungsi Remove video dari Queue dipilih.")
-    elif choice == 6:
-      print("Terima kasih sudah menggunakan StreamTree. Sampai jumpa!")
-      return False
-    else:
-      print("Pilihan tidak valid. Silakan coba lagi.")
-  except ValueError:
-    print("Input tidak valid")
-  return True
+  videos.printAllTree()
+  print(recommendation)
+
+  while(True):
+    try:
+      choice = int(input("Pilih menu : "))
+      if choice == 1:
+        print("Fungsi Add video ke StreamTree.")
+        title = input()
+        tags = list(map(str, input().split(' ')))
+        description = input()
+        duration = int(input())
+        
+        video = Video(title,tags,description,duration)
+        
+        videos.insert(video)
+        recommendation.addVideo(video)
+        
+        videos.printAllTree()
+        print(recommendation)
+        
+      elif choice == 2:
+        print("Fungsi Remove video SreamTree.")
+      elif choice == 3:
+        print("Fungsi Find recommended dari video.")
+      elif choice == 4:
+        print("Fungsi Add video ke Queue.")
+      elif choice == 5:
+        print("Fungsi Remove video dari Queue.")
+      elif choice == 6:
+        print("Terima kasih sudah menggunakan StreamTree. Sampai jumpa!")
+        return False
+      else:
+        print("Pilihan tidak valid. Silakan coba lagi.")
+    except ValueError:
+      print("Input tidak valid")
 
 if __name__ == "__main__":
-  while main_menu():
-    print("\n")
+  main_menu()
+  
