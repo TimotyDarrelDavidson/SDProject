867da82f2ac287af6c5862cb47426dd1c2f8a27b
TimotyDarrelDavidson <c14230235@john.petra.ac.id>
2024-12-18 17:02:19 +0700

    udah selesai tak benerin, kalau mau diubah bilang ke aku saja

--- Graph.py
+++ Graph.py
@@ -1,55 +1,59 @@
-class Video:
-
-    def __init__(self,title,tags,description,duration):
-        self.title = title
-        self.tags = tags
-        self.description = description
-        self.duration = duration
-        self.left = None
-        self.right = None
-        
+from Video import Video
 
 class Recommend:
-    def __init__(self, Video):
+    def __init__(self, Video: Video):
         self.title = Video.title
         self.tags = Video.tags
+        self.left = None
+        self.right = None
+
+class Edge:
+    def __init__(self, recommend: Recommend):
+        self.title = recommend.title
+        self.left = recommend
         self.right = None
 
 class Recommendation:
     def __init__(self):
         self.head = None
     
-    def addVideo(self,Video):
+    def addVideo(self,video: Video):
         
         if self.head is None:
-            self.head = Video
+            self.head = Recommend(video)
             return
         
+        node = Recommend(video)
         temp = self.head
-        while temp:
-            if set(temp.tags) & set(Video.tags):
-                node = Recommend(Video)
-                if temp.right:
-                    self.addEdge(temp.right,node)
+        
+        def link_nodes(src: Recommend, dest: Recommend):
+            if set(src.tags) & set(dest.tags):  # Check for common tags
+                if src.right:
+                    self.addEdge(src, dest)
                 else:
-                    temp.right = node
+                    src.right = Edge(dest)
+                self.addEdge(dest, src)
+        
+        while temp.left:
+            link_nodes(temp,node)
             temp = temp.left
         
-        temp = self.head
-        while temp.left:
-            temp = temp.left
-            
-        temp.left = Video
+        link_nodes(node,temp)
+        temp.left = node
             
                 
-    def addEdge(self, Recommend, newRecommend):
+    def addEdge(self, Recommend: Recommend, newRecommend: Recommend):
+        if(Recommend.right is None):
+            Recommend.right = Edge(newRecommend)
+            return
+        
         temp = Recommend
         while temp.right:
             temp = temp.right
         
-        temp.right = newRecommend
-        
-    def getAllEdge(self,Recommend):
+        temp.right = Edge(newRecommend)
+                
+    def getAllEdge(self,Recommend: Edge):
         temp = Recommend
         s = " -> "
         while temp:
@@ -69,16 +73,16 @@ class Recommendation:
         return s
         
         
-video1 = Video("Action Movie 1", ["action", "adventure"], "An exciting action movie", 120)
-video2 = Video("Action Movie 2", ["action", "adventure"], "Another thrilling action movie", 125)
-video3 = Video("Comedy Movie 1", ["comedy", "family"], "A funny family movie", 100)
-video4 = Video("Thriller Movie 1", ["thriller", "action"], "A suspenseful action thriller", 110)
+# video1 = Video("Action Movie 1", ["action", "adventure"], "An exciting action movie", 120)
+# video2 = Video("Action Movie 2", ["action", "adventure"], "Another thrilling action movie", 125)
+# video3 = Video("Comedy Movie 1", ["comedy", "family"], "A funny family movie", 100)
+# video4 = Video("Thriller Movie 1", ["thriller", "action"], "A suspenseful action thriller", 110)
 
-# Create the recommendation system
-rec_sys = Recommendation()
-rec_sys.addVideo(video1)
-rec_sys.addVideo(video2)
-rec_sys.addVideo(video3)
-rec_sys.addVideo(video4)
+# # Create the recommendation system
+# rec_sys = Recommendation()
+# rec_sys.addVideo(video1)
+# rec_sys.addVideo(video2)
+# rec_sys.addVideo(video3)
+# rec_sys.addVideo(video4)
 
-print(rec_sys)
+# print(rec_sys)
